04f9dfa75a7ceb6131a0ce66c14e69f08d8ff199
Paloma Araujo <paclaraujo@gmail.com>
2024-07-03 13:55:09 -0300

    feat: add registration context

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export interface Registration {
+  id?: number;
+  employeeName: string;
+  email: string;
+  admissionDate: string;
+  cpf: string;
+  status: 'REVIEW' | 'REPROVED' | 'APPROVED';
+}
+
+export interface RegistrationContextType {
+  registrations: Registration[];
+  setRegistrations: React.Dispatch<React.SetStateAction<Registration[]>>;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
